d3816bc9edf63386e21a8e1d690826bf049fbd6c
seacat271 <seacat271083@gmail.com>
2022-05-31 15:56:28 +0300

    Update while.js

--- while.js
+++ while.js
@@ -1,8 +1,17 @@
 
 alert ("Угадай число")
 let count
-count=17
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+count=getRandomInt(1, 30)
 let quest
-do quest=prompt ("Введи число от 1 до 30");
+do {quest=prompt ("Введи число от 1 до 30");
+if (quest<count) {
+    alert ("Маловато будет")
+} else if (quest>count) {
+    alert ("Не, ну это много")
+}}
 while (count!=quest)
+
 alert ("УГАДАЛА!! Возьми с полки пирожок");
